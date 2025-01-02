052e74e449888811f60ef3d4dd95334bbe6953bf
priyanshics20 <apriyanshi637@gmail.com>
2025-01-02 16:17:42 +0530

    react-code pushed

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" style={{margin:'auto', fontSize:20 , color:'red', fontWeight:600}}>
+      <div>Hello </div>
+      <div>Here we will try to build a docker image</div>
+    </div>
+  );
+}
+
+export default App;
