02272b99c1bbde645f47bda93a70f261c30165cd
priyanshics20 <apriyanshi637@gmail.com>
2025-01-03 16:01:08 +0530

    fix the username issue

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App" style={{margin:'auto', fontSize:20 , color:'red', fontWeight:600}}>
       <div>Hello </div>
-      <div>Here we will try to build a docker image</div>
+      <div>Here we will try to build a docker images </div>
       <div> I have done CI/CD pipeline</div>
     </div>
   );
